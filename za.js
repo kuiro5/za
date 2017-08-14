ad70c3e664c2af25236cc05d96eda3265a772a94
kuiros <joshkuiros@gmail.com>
2017-08-13 21:36:33 -0700

    Clean up

--- za.js
+++ za.js
@@ -1,59 +1,51 @@
 const post = async ({ url, headers = '', data = {} }) => {
-  return new Promise((resolve, reject) => {
-    fetch(url, {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers
-    }).then((response) => {
-      if (response.status >= 400 && response.status < 600) {
-        return reject(response.status, response.json());
-      }
-      return resolve(response.json());
-    })
-  });
+  return fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers
+  }).then((response) => {
+    if (response.status >= 400 && response.status < 600) {
+      return Promise.reject(response.status, response.json());
+    }
+    return Promise.resolve(response.json());
+  })
 }
 
 const put = async ({ url, headers = '', data = {} }) => {
-  return new Promise((resolve, reject) => {
-    fetch(url, {
-      method: 'PUT',
-      body: JSON.stringify(data),
-      headers
-    }).then((response) => {
-      if (response.status >= 400 && response.status < 600) {
-        return reject(response.status, response.json());
-      }
-      return resolve(response.json());
-    })
-  });
+  return fetch(url, {
+    method: 'PUT',
+    body: JSON.stringify(data),
+    headers
+  }).then((response) => {
+    if (response.status >= 400 && response.status < 600) {
+      return Promise.reject(response.status, response.json());
+    }
+    return Promise.resolve(response.json());
+  })
 }
 
 const get = async ({ url, headers = '' }) => {
-  return new Promise((resolve, reject) => {
-    fetch(url, {
-      method: 'GET',
-      headers,
-    }).then((response) => {
-      if (response.status >= 400 && response.status < 600) {
-        return reject(response.status, response.json());
-      }
-      return resolve(response.json());
-    })
-  });
+  return fetch(url, {
+    method: 'GET',
+    headers,
+  }).then((response) => {
+    if (response.status >= 400 && response.status < 600) {
+      return Promise.reject(response.status, response.json());
+    }
+    return Promise.resolve(response.json());
+  })
 }
 
 const destroy = async ({ url, headers ='' }) => {
-  return new Promise((resolve, reject) => {
-    fetch(url, {
-      method: 'DELETE',
-      headers,
-    }).then((response) => {
-      if (response.status >= 400 && response.status < 600) {
-        return reject(response.status, response.json());
-      }
-      return resolve(response.json());
-    })
-  });
+  return fetch(url, {
+    method: 'DELETE',
+    headers,
+  }).then((response) => {
+    if (response.status >= 400 && response.status < 600) {
+      return Promise.reject(response.status, response.json());
+    }
+    return Promise.resolve(response.json());
+  })
 }
 
 export default { get, put, post, destroy };
